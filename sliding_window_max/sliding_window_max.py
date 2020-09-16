6dc15e78cd6043ae9c559c4b28c102e597f5d22a
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-09-16 17:55:37 -0600

    add sliding window

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,15 +2,27 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
 
-    pass
+
+def sliding_window_max(nums, k):
+    output = []
+    for i in range(len(nums)-k+1):
+        window = nums[i:i+k]
+        largest = False
+        for item in window:
+            if not largest:
+                largest = item
+            if item > largest:
+                largest = item
+        output.append(largest)
+    print(output)
+    return output
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
